fa250f73f35914b76d2829aef55e6508071433e8
Gabrielopx <146769808+Gabrielopx@users.noreply.github.com>
2023-10-31 00:50:54 -0300

    Update ping.js

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -1,7 +1,49 @@
+const Discord = require("discord.js")
+
+
+
 module.exports = {
-    name: 'ping',
-    aliases: ['latencia'],
+
+    name: "ping", // Coloque o nome do seu comando
+
+    aliases: [""], // Coloque sinônimos do nome do comando
+
+
+
     run: async(client, message, args) => {
-        message.reply(`🏓 | Minha latencia é de \`${client.ws.ping}ms\``)
+
+
+
+        let embed = new Discord.EmbedBuilder()
+
+        .setColor("Random")
+
+        .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL() })
+
+        .setDescription(`🏓 Olá ${message.author}, seu ping está em: \`carregando...\`.`);
+
+
+
+        let embed2 = new Discord.EmbedBuilder()
+
+        .setColor("Random")
+
+        .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL() })
+
+        .setDescription(`🏓 Olá ${message.author}, seu ping está em: \`${client.ws.ping}ms\`.`);
+
+
+
+        message.reply({ embeds: [embed] }).then(msg => {
+
+            setTimeout( () => {
+
+                msg.edit({ embeds: [embed2] })
+
+            }, 3000)
+
+        })
+
     }
+
 }
